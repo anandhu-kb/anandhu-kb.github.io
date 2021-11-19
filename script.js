6e2d00ba9d0310c10bc09df91c2ac47519bc0131
anandhu-kb <56553335+anandhu-kb@users.noreply.github.com>
2021-11-19 09:38:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+window.onload = () => {
+  const myInput = document.getElementById("dummyKeyboard");
+  myInput.onpaste = (e) => e.preventDefault();
+};
+const COMMANDS = {
+  help: 'Supported commands: <span class="code">about</span>, <span class="code">experience</span>, <span class="code">education</span>, <span class="code">skills</span>, <span class="code">contact</span>',
+  about:
+    "Hello 👋<br>I'm Anandhu K B. I’m a 18 yr old pentester and bug bounty hunter currently living in India.",
+  skills:
+    '<span class="code">Skill:</span> Ethical Hacking, Pen-Testing, Bug Hunting,..<br>',
+  education:
+    "Christ College Puliyanmala, Idukki, Kerala, India<br> BCA , 1st Year",
+  
+  experience: "No Experience😥",
+
+  contact:
+    "You can contact me on any of following links:<br><a href='https://t.me/d4rk4c3' class='success link'>Telegram</a>, <a href='https://www.instagram.com/d4rk.4c3/' class='success link'>Instagram</a>, <a href='https://twitter.com/d4rk_4c3' class='success link'>Twitter</a>",
+};
+
+const userInput = document.getElementById("userInput");
+const terminalOutput = document.getElementById("terminalOutput");
+const inputfield = document.getElementById("dummyKeyboard");
+
+inputfield.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    let input = e.target.value;
+    input = input.toLowerCase();
+    if (input.length === 0) {
+      return;
+    }
+    let output;
+    output = `<div class="terminal-line"><span class="success">➜</span> <span class="directory">~</span> ${input}</div>`;
+    if (!COMMANDS.hasOwnProperty(input)) {
+      output += `<div class="terminal-line">no such command: ${input}</div>`;
+      console.log("Oops! no such command");
+    } else {
+      output += COMMANDS[input];
+    }
+    terminalOutput.innerHTML = `${terminalOutput.innerHTML}<div class="terminal-line">${output}</div>`;
+    terminalOutput.scrollTop = terminalOutput.scrollHeight;
+    e.target.value = "";
+  }
+});

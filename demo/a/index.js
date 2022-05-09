b2e3effd8bb7f58882bdccd0a5ade822b55fb77a
anandhu-kb <56553335+anandhu-kb@users.noreply.github.com>
2022-05-09 12:13:49 +0530

    Create index.js

---
+++ demo/a/index.js
@@ -0,0 +1,16 @@
+let countEl = document.getElementById("count-el")
+let count=0
+function increment(){
+    count=count+1
+    countEl.innerText=count
+}
+
+
+
+
+let ag=0
+function age(){
+
+    ag=ag+1
+    document.getElementById("age-al").innerText=ag
+}

0adcf91053ee1dc25bc7f7e3138c80986c8314cb
anandhu-kb <56553335+anandhu-kb@users.noreply.github.com>
2023-06-06 21:29:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ window.onload = () => {
   const myInput = document.getElementById("dummyKeyboard");
   myInput.onpaste = (e) => e.preventDefault();
 };
+
 const COMMANDS = {
-  help: 'Supported commands: <span class="code">about</span>, <span class="code">experience</span>, <span class="code">education</span>, <span class="code">skills</span>, <span class="code">contact</span>',
+  help: 'Supported commands: <span class="code">about</span>, <span class="code">experience</span>, <span class="code">education</span>, <span class="code">skills</span>, <span class="code">contact</span>, <span class="code">ls</span>',
+  ls: "<a href='./project' target='blank'>projects</a>",
+  projects: "window.open('./project/index.html', '_blank');",
   about:
     "Hello 👋<br>I'm Anandhu K B. I’m a 20 yr old pentester and bug bounty hunter currently living in India.",
   skills:
@@ -14,7 +17,7 @@ const COMMANDS = {
   experience: "No Experience😥",
 
   contact:
-    "You can contact me on any of following links:<br><a href='https://t.me/d4rk4c3' class='success link'>Telegram</a>, <a href='https://www.instagram.com/d4rk.4c3/' class='success link'>Instagram</a>, <a href='https://twitter.com/d4rk_4c3' class='success link'>Twitter</a>",
+    "You can contact me on any of following links:<br><a href='https://t.me/d4rk4c3' class='success link'>Telegram</a>, <a href='https://www.instagram.com/anandh.uh_/' class='success link'>Instagram</a>, <a href='https://twitter.com/d4rk_4c3' class='success link'>Twitter</a>",
 };
 
 const userInput = document.getElementById("userInput");
@@ -34,7 +37,11 @@ inputfield.addEventListener("keypress", (e) => {
       output += `<div class="terminal-line">no such command: ${input}</div>`;
       console.log("Oops! no such command");
     } else {
-      output += COMMANDS[input];
+      if (input === "projects") {
+        eval(COMMANDS[input]);
+      } else {
+        output += COMMANDS[input];
+      }
     }
     terminalOutput.innerHTML = `${terminalOutput.innerHTML}<div class="terminal-line">${output}</div>`;
     terminalOutput.scrollTop = terminalOutput.scrollHeight;
